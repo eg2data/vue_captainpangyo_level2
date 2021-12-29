1078871d8f3f1a5f4aebaddb9233fff921a7dc66
eg2data <eg2data@gmail.com>
2021-12-29 14:37:19 +0900

    29th December, finished.

---
+++ vue-news-ts/src/views/ListView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ListItem :items="newsItems"></ListItem>
+  </div>
+</template>
+
+<script>
+import ListItem from '../components/ListItem.vue';
+import {fetchNews} from "@/api";
+
+export default {
+  components: {
+    ListItem
+  },
+
+  data() {
+    return {
+      newsItms: []
+    }
+  },
+  
+  methods: {
+    async fetchNewsItems() {
+      const response = await fetchNews();
+      console.log(response.data)
+      this.newsItms = response.data
+    }
+  },
+
+  created() {
+    this.fetchNewsItems();
+  }
+}
+</script>
+
+<style>
+
+</style>
